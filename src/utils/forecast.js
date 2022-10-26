6209e33f09a731a1d8339c59f75c36ef873551dc
Nadia Tsygankova <47821694+nadezdatsygankova@users.noreply.github.com>
2022-10-26 06:51:19 -0400

    new key

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('postman-request');
 
 const forecast = (location, callback) => {
-  const url =`http://api.weatherstack.com/current?access_key=b56246cc0e78ebcb09a047f7606948a7&query=${encodeURIComponent(location)}`;
+  const url =`http://api.weatherstack.com/current?access_key=7a62abc011b94ad21b8c61a73f8636ea&query=${encodeURIComponent(location)}`;
   request({url, json: true}, (error, {body} ={})=>{
     if(error){
       callback('Unable to connect to weather service', undefined);
